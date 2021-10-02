13b28500f624ada1db2960a22dab19bc45317821
josedsj <josealejandro98@gmail.com>
2021-10-02 12:06:55 -0500

    Add files via upload

---
+++ Inferimiento de Género Según Nombre.R
@@ -0,0 +1,8 @@
+df=data.frame(nombres=c("Ana","Andrea","Julián","Christian","Paula"),
+              edad=c(15,21,42,8,35));df
+install.packages("genero")
+library(genero)
+genero("paul")
+genero(c("Ximena","Arturo"))
+genero(df["nombres"])
+genero("maria jose",lang = "es")
